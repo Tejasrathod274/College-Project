c9f0a3c9e7a93cc7aa1dd25a47b3a43bc06994a7
Tejas Rathod <rathodtejas302@gmail.com>
2026-02-14 16:42:20 +0530

    fixed railway start

--- app.py
+++ app.py
@@ -804,7 +804,8 @@ def confirm_booking(booking_id):
     return redirect(url_for('guide_dashboard'))
 
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
+
